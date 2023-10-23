db7134016d0402df2b833e92465dfea43006fe26
Samm1911 <s_gostanian@hotmail.com>
2023-10-22 22:58:56 -0400

    Password Generator

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # 3-Password-Generator
 
 ## Description
+
 Welcome to my random password generator. This allows you to create a function, based on the criteria you choose.
 
-Here is a link to the password generator: [Password Generator]()
+Here is a link to the password generator: [Password Generator](https://github.com/Samm1911/3-Password-Generator/)
+
+## Usage
+
+It's very easy, just click the "Generate Password" Button
+and choose your password length between 8-128.
+
+Next, you choose your four options.
+After that is done, you can enjoy your random password.
--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 // Create the function final password for creating the password type otion, and create empty password string.
 function finalPassword(
   length,
-  lowercaseType,
-  uppercaseType,
-  numericType,
+  lowerCaseType,
+  upperCaseType,
+  NumericType,
   specialCharsType
 ) {
   // create variables for each type and a random empty password
@@ -12,17 +12,17 @@ function finalPassword(
   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numericCharacters = "0123456789";
   const specialCharacters = "/^[!@#$%^&*()_+-=[]{};':|.<,>/?]*$/";
-  var randomPassword = "";
+  var randomPassword = " ";
 
   // use concat to add each type to the random password string.
-  if (lowercaseType) {
+  if (lowerCaseType) {
     randomPassword = randomPassword.concat(lowerCase);
   }
 
-  if (uppercaseType) {
+  if (upperCaseType) {
     randomPassword = randomPassword.concat(upperCase);
   }
-  if (numericType) {
+  if (NumericType) {
     randomPassword = randomPassword.concat(numericCharacters);
   }
   if (specialCharsType) {
@@ -35,38 +35,40 @@ function finalPassword(
   // interate the loop to through all the added options to random password.
 
   for (let index = 0; index < length; index++) {
-    const passwordIndex = Math.floor(Math, random() * randomPassword, length);
+    const passwordIndex = Math.floor(Math.random() * randomPassword.length);
     password += randomPassword[passwordIndex];
   }
   // return password with optoins chosen
   return password;
 }
-
 // ask user for lenght
 // choose lenght to be between 8 and 128 characters
 // prompt user for lenght
 // generate password function
 function generatePassword() {
   // prompt user to choose a password lenght
-  var choosePasswordLenght = prompt(
-    "choose the lenght of youur password (between 8-128 characters)"
+  var choosePasswordLength = prompt(
+    "choose the lenght of your password (between 8-128 characters)"
   );
   // convert inout into integer
-  choosePasswordLenght = parseInt(choosePasswordLenght);
+  choosePasswordLength = parseInt(choosePasswordLength);
   // if the input is not a number or not between 8-128 it will show an alert of the input being invalid
   if (
-    isNaN(choosePasswordLenght) ||
-    choosePasswordLenght < 8 ||
-    choosePasswordLenght > 128
+    isNaN(choosePasswordLength) ||
+    choosePasswordLength < 8 ||
+    choosePasswordLength > 128
   ) {
+    alert(
+      "not a valid password length, the password length should be between 8-128"
+    );
   } else {
     // choose incase lowercase
-    const lowercaseType = confirm(
+    const lowerCaseType = confirm(
       "Do you want Lowercase letters in your password?"
     );
 
     // choose incase uppercase
-    const uppercaseType = confirm(
+    const upperCaseType = confirm(
       "Do you want Uppercase letters in your password?"
     );
 
@@ -77,34 +79,34 @@ function generatePassword() {
     const specialCharsType = confirm(
       "Do you want Special Characters in your password?"
     );
-  }
 
-  // make sure to have one of those options at least
-  if (!lowercaseType && !uppercaseType && !numericType && !specialCharsType) {
-    window, alert("Not valid, choose at least one of the options available");
-  } else {
-    // Password must match all of the given criteria
-    // password must be displayed in an alert or written to that page.
-    const password = finalPassword(
-      choosePasswordLenght,
-      lowercaseType,
-      uppercaseType,
-      numericType,
-      specialCharsType
-    );
+    // make sure to have one of those options at least
+    if (!lowerCaseType && !upperCaseType && !NumericType && !specialCharsType) {
+      window.alert("Not valid, choose at least one of the options available");
+    } else {
+      // Password must match all of the given criteria
+      // password must be displayed in an alert or written to that page.
+      const password = finalPassword(
+        choosePasswordLength,
+        lowerCaseType,
+        upperCaseType,
+        NumericType,
+        specialCharsType
+      );
 
-    // return complete generated password
-    return password;
+      // return complete generated password
+      return password;
+    }
   }
 }
 
 // Get references to the #generate element
-var generateBtn = Document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = Document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
